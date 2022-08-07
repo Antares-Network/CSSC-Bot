5625ba0e0c5da20bf29c44e2218cee53f4bcde7e
Nate Goldsborough <nathen418@playantares.com>
2022-08-07 09:26:37 -0500

    Capitalized all role names

--- definitions.ts
+++ definitions.ts
@@ -4,37 +4,38 @@ type dict = {
 
 function returnRoles(): dict[] {
 	let studentRoleDictionary = {
-		prefrosh: String(process.env.prefrosh),
-		freshman: String(process.env.freshman),
-		sophomore: String(process.env.sophomore),
-		junior: String(process.env.junior),
-		senior: String(process.env.senior),
-		graduatestudent: String(process.env.graduatestudent),
-		alumni: String(process.env.alumni),
+		Prefrosh: String(process.env.prefrosh),
+		Freshman: String(process.env.freshman),
+		Sophomore: String(process.env.sophomore),
+		Junior: String(process.env.junior),
+		Senior: String(process.env.senior),
+		Graduatestudent: String(process.env.Graduatestudent),
+		Alumni: String(process.env.alumni),
 	};
 	let staffRoleDictionary = {
-		tutor: String(process.env.tutor),
-		sileader: String(process.env.sileader),
-		ta: String(process.env.ta),
-		studentemployee: String(process.env.studentemployee)
+		Tutor: String(process.env.tutor),
+		Sileader: String(process.env.sileader),
+		Ta: String(process.env.ta),
+		Studentemployee: String(process.env.Studentemployee),
+		Professor: String(process.env.Professor)
 	}
 	if (process.env.debugMode === "true") {
 		
 		let studentRoleDictionary = {
-			prefrosh: "1003849993255403601",
-			freshman: "1003367383181824131",
-			sophomore: "1003367410679685262",
-			junior: "1003367473053171743",
-			senior: "1003850030920245409",
-			graduatestudent: "1003850131994583122",
-			alumni: "1003850162935975960"
+			Prefrosh: "1003849993255403601",
+			Freshman: "1003367383181824131",
+			Sophomore: "1003367410679685262",
+			Junior: "1003367473053171743",
+			Senior: "1003850030920245409",
+			Graduatestudent: "1003850131994583122",
+			Alumni: "1003850162935975960"
 		};
 		let staffRoleDictionary = {
-			tutor: "1003850195202744560",
-			sileader: "1003850241591742595",
-			ta: "1003850228643934260",
-			studentemployee: "1003850274840002620",
-			professor: "1003881218686865439"
+			Tutor: "1003850195202744560",
+			Sileader: "1003850241591742595",
+			Ta: "1003850228643934260",
+			Studentemployee: "1003850274840002620",
+			Professor: "1003881218686865439"
 		}
 		return [studentRoleDictionary, staffRoleDictionary];
 	};
