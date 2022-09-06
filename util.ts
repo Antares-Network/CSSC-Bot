5bd4c69ebe6fad5dd491f753f6ed4eaa97f652ed
John Schiltz <johnschiltz2000@gmail.com>
2022-09-05 22:32:44 -0500

    remove Function type

--- util.ts
+++ util.ts
@@ -1,4 +1,4 @@
 export function sleep(ms: number) {
   // Create new promise that resolves itself after a delay of <ms>
-  return new Promise((resolve: Function) => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
