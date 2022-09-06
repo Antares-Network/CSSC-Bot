29b870111cc121f75dfe4f8fbccef82712a6d5d3
John Schiltz <johnschiltz2000@gmail.com>
2022-09-05 22:57:59 -0500

    try callback for resolve

--- util.ts
+++ util.ts
@@ -1,4 +1,8 @@
 export function sleep(ms: number) {
   // Create new promise that resolves itself after a delay of <ms>
-  return new Promise((resolve) => setTimeout(resolve, ms));
+  return new Promise((resolve) =>
+    setTimeout(function () {
+      resolve;
+    }, ms)
+  );
 }
