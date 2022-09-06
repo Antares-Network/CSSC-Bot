a5a6a983a0f415eba186aea5e49db996027af6c9
John Schiltz <johnschiltz2000@gmail.com>
2022-09-05 23:23:13 -0500

    rewrite wait to specify resolve type

--- util.ts
+++ util.ts
@@ -1,8 +1,6 @@
 export function sleep(ms: number) {
   // Create new promise that resolves itself after a delay of <ms>
-  return new Promise((resolve) =>
-    setTimeout(function () {
-      resolve;
-    }, ms)
+  return new Promise((resolve: (args: void) => void) =>
+    setTimeout(resolve, ms)
   );
 }
