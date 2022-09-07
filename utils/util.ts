b0cd0ffec1e983f8ac0072217f07a87b64356233
nathen418 <nathen418@users.noreply.github.com>
2022-09-07 21:28:28 +0000

    Apply auto formatting changes

--- utils/util.ts
+++ utils/util.ts
@@ -1,4 +1,4 @@
-import fs from 'node:fs';
+import fs from "node:fs";
 export function sleep(ms: number) {
   // Create new promise that resolves itself after a delay of <ms>
   return new Promise((resolve: (args: void) => void) =>
@@ -6,33 +6,32 @@ export function sleep(ms: number) {
   );
 }
 
-
 //! This code was taken from the package `is-docker` and modified to work here with esm
 //! Original repository: https://github.com/sindresorhus/is-docker
 let isDockerCached: boolean;
 
 function hasDockerEnv() {
-    try {
-        fs.statSync('/.dockerenv');
-        return true;
-    } catch {
-        return false;
-    }
+  try {
+    fs.statSync("/.dockerenv");
+    return true;
+  } catch {
+    return false;
+  }
 }
 
 function hasDockerCGroup() {
-    try {
-        return fs.readFileSync('/proc/self/cgroup', 'utf8').includes('docker');
-    } catch {
-        return false;
-    }
+  try {
+    return fs.readFileSync("/proc/self/cgroup", "utf8").includes("docker");
+  } catch {
+    return false;
+  }
 }
 
 export function isDocker() {
-    // TODO: Use `??=` when targeting Node.js 16.
-    if (isDockerCached === undefined) {
-        isDockerCached = hasDockerEnv() || hasDockerCGroup();
-    }
+  // TODO: Use `??=` when targeting Node.js 16.
+  if (isDockerCached === undefined) {
+    isDockerCached = hasDockerEnv() || hasDockerCGroup();
+  }
 
-    return isDockerCached;
+  return isDockerCached;
 }
