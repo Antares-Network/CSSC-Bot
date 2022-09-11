652990532a5cd41b6e0d7d749239e609b3b35c52
John Schiltz <johnschiltz2000@gmail.com>
2022-09-10 21:29:35 -0500

    Update cleanString

--- utils/channelUtils.ts
+++ utils/channelUtils.ts
@@ -4,9 +4,9 @@ import chalk from "chalk";
 export function cleanChannelString(s: string): string {
   return s
     .toLowerCase()
-    .replace(/[`~!@#$%^&*())|+=?;:'",.<>\{\}\[\]\\\/]/gi, "")
+    .replace(/[`~!@#$%^&*))|+=?;:'",.<>\{\}\[\]\\\/]/gi, "")
     .replace("compsci ", "cs")
-    .replace(" ", "-");
+    .replace(/[ (]/gi, "-");
 }
 
 export function checkForChannel(guild: Guild, channel_name: string) {
