d60803a95ffd31ef21b801a33aa71b3f7f9f9923
John Schiltz <johnschiltz2000@gmail.com>
2022-09-15 22:29:46 -0500

    group contiguous characters

--- utils/roleUtils.ts
+++ utils/roleUtils.ts
@@ -13,7 +13,7 @@ export interface IRole {
 export function cleanRoleString(role_name: string): string {
   const clean_role_name: string = role_name
     .toLowerCase()
-    .replace(/[`~!@#$%^&*()|+=?;:'",.<>\{\}\[\]\\\/]/gi, "")
+    .replace(/[`~!@#$%^&*()|+=?;:'",.<>\{\}\[\]\\\/]+/gi, "")
     .replace(/\s+/gi, "-")
     .replace(/-{2,}/gi, "-");
   return clean_role_name;
