24adda7035daa518ce2a19f536a3c7e6138a3fd8
nathen418 <nathen418@users.noreply.github.com>
2023-01-20 19:55:39 +0000

    Apply auto formatting changes

--- src/models/classModel.ts
+++ src/models/classModel.ts
@@ -2,27 +2,27 @@ import mongoose, { Schema } from "mongoose";
 import { IRole } from "../utils/roles";
 
 export interface IClass extends IRole {
-	id: string;
-	NAME: string;
-	TITLE: string;
-	INFO: string;
-	DUPE: boolean;
-	ACTIVE: boolean;
-	ROLE_NAME: string;
-	ROLE_ID: string;
-	CHANNEL_ID: string;
+  id: string;
+  NAME: string;
+  TITLE: string;
+  INFO: string;
+  DUPE: boolean;
+  ACTIVE: boolean;
+  ROLE_NAME: string;
+  ROLE_ID: string;
+  CHANNEL_ID: string;
 }
 
 const ClassSchema = new Schema({
-	id: { type: String },
-	NAME: { type: String, required: true },
-	TITLE: { type: String, required: true },
-	INFO: { type: String, required: true },
-	DUPE: { type: Boolean, required: false },
-	ACTIVE: { type: Boolean, required: true },
-	ROLE_NAME: { type: String, required: false },
-	ROLE_ID: { type: String, required: false },
-	CHANNEL_ID: { type: String, required: false },
+  id: { type: String },
+  NAME: { type: String, required: true },
+  TITLE: { type: String, required: true },
+  INFO: { type: String, required: true },
+  DUPE: { type: Boolean, required: false },
+  ACTIVE: { type: Boolean, required: true },
+  ROLE_NAME: { type: String, required: false },
+  ROLE_ID: { type: String, required: false },
+  CHANNEL_ID: { type: String, required: false },
 });
 
 const name = "class";
